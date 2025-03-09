ee16201b481c4d3b348e7fd64f37e0559908d84a
SolWatermelon <iamwatermelon0430@gmail.com>
2025-03-10 04:05:35 +0800

    [feat] connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose"
+import dotenv from "dotenv";
+dotenv.config();
 const app = express()
+
+mongoose.connect(process.env.MONGODB).then(() => {
+console.log("connected to mongoDB!");
+}).catch((e) => {
+    console.log("failed to connect to mongoDB:", e);
+})
 
 
 app.listen(3000, () => {
