c90ef164a729c3405c993960eec7cb3e3a944dd9
SolWatermelon <iamwatermelon0430@gmail.com>
2025-03-14 14:58:04 +0800

    [fix] beautify Profile page-1

--- client/src/routes/Profile.jsx
+++ client/src/routes/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { ErrorMessage } from "@hookform/error-message";
@@ -10,10 +10,20 @@ import {
   useQueryClient,
 } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useSelector, useDispatch } from "react-redux";
+import { MdOutlineEdit } from "react-icons/md";
 
 const Profile = () => {
+  const avatarRef = useRef(null);
   const {
     register,
     handleSubmit,
@@ -25,6 +35,10 @@ const Profile = () => {
   // const [formdata, setFormData] = useState(null);
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+
+  const handleAvatar = () => {
+    avatarRef.current.click();
+  };
 
   const mutation = useMutation({
     mutationFn: (userSigninData) => signUp(userSigninData),
@@ -45,130 +59,263 @@ const Profile = () => {
   };
 
   return (
-    <Tabs defaultValue="edit" className="flex flex-col items-start">
-      <TabsList className="grid grid-cols-2">
-        <TabsTrigger value="edit">編輯資料</TabsTrigger>
-        <TabsTrigger value="account">帳戶與文章</TabsTrigger>
-        {/* <TabsTrigger value="article">文章管理</TabsTrigger> */}
-      </TabsList>
-      <TabsContent className="w-full mb-24 " value="edit">
-        <div className="flex tablet:flex-row flex-col justify-center items-center tablet:items-start box-content bg-white rounded-lg shadow-md">
-          {/* 放大頭照 */}
+    <div className="flex justify-center mt-12">
+      <Tabs defaultValue="edit" className="flex flex-col items-start">
+        <TabsList className="grid grid-cols-2">
+          <TabsTrigger value="edit">編輯資料</TabsTrigger>
+          <TabsTrigger value="accountandarticle">帳戶與文章</TabsTrigger>
+          {/* <TabsTrigger value="article">文章管理</TabsTrigger> */}
+        </TabsList>
+        <TabsContent className=" mb-24 xl:w-[700px] " value="edit">
+          <div className="flex tablet:flex-row flex-col justify-center items-center tablet:items-start box-content bg-white rounded-lg shadow-md">
+            {/* 放大頭照 */}
 
-          <span className="px-12 pt-12 md:p-12">
-            <img
-              className="w-40 rounded-full object-cover bg-gray-300 flex items-center justify-center relative"
-              src={currentUser?.avatar}
-              alt="profile_pic"
+            <span className="px-12 pt-12 md:p-12 relative">
+              <img
+                onClick={handleAvatar}
+                className="cursor-pointer hover:ring-4 w-40 rounded-full object-cover bg-gray-300 flex items-center justify-center relative"
+                src={currentUser?.avatar}
+                alt="profile_pic"
+              />
+              <MdOutlineEdit className="absolute top-32 right-16 text-2xl text-white" />
+            </span>
+
+            <input
+              className="hidden"
+              ref={avatarRef}
+              id="avatar"
+              type="file"
+              placeholder="avatar"
+              accept="image/*"
             />
-          </span>
 
-          {/* <div> */}
-          <div className="flex items-center justify-center mt-[10px]">
-            <div className="px-8 pt-3 pb-16 md:p-8 w-full max-w-md">
-              <h1 className="text-4xl font-bold text-gray-700 mb-8">Sign Up</h1>
+            {/* <div> */}
+            <div className="flex items-center justify-center mt-[10px] w-full">
+              <div className="px-8 pt-3 pb-16 md:p-8 w-full max-w-md">
+                <h1 className="text-4xl font-bold text-gray-700 mb-8">
+                  Sign Up
+                </h1>
 
-              {mutation.isError && (
-                <p className="text-red-500 text-sm">{"信箱已被使用，請更改"}</p>
-              )}
+                {mutation.isError && (
+                  <p className="text-red-500 text-sm">
+                    {"信箱已被使用，請更改"}
+                  </p>
+                )}
 
-              {mutation.isSuccess && (
-                <p className="text-blue-500 text-sm">{"成功修改資料！"}</p>
-              )}
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="mb-4">
-                  <input
-                    id="name"
-                    type="text"
-                    placeholder="name"
-                    className="w-full p-3 rounded-full border-2 border-gray-300 text-gray-800 focus:outline-none focus:border-darkorange"
-                    {...register("name", { required: "name is required." })}
-                  />
-                  <ErrorMessage
-                    errors={errors}
-                    name="name"
-                    as="p"
-                    className="text-red-500 text-sm"
-                  />
-                </div>
+                {mutation.isSuccess && (
+                  <p className="text-blue-500 text-sm">{"成功修改資料！"}</p>
+                )}
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  {/* <div className="hidden mb-4">
+                    <input
+                    ref={avatarRef}
+                      id="avatar"
+                      type="file"
+                      placeholder="avatar"
+                      {...register("avatar")}
+                    />
+                    <ErrorMessage
+                      errors={errors}
+                      name="avatar"
+                      as="p"
+                    />
+                  </div> */}
 
-                <div className="mb-4">
-                  <input
-                    id="email"
-                    type="email"
-                    placeholder="email"
-                    className="w-full p-3 rounded-full border-2 border-gray-300 text-gray-800 focus:outline-none focus:border-darkorange"
-                    {...register("email", {
-                      required: "Email is required.",
-                      pattern: {
-                        value:
-                          /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                        message: "Email format is incorrect.",
-                      },
-                    })}
-                  />
-                  <ErrorMessage
-                    errors={errors}
-                    name="email"
-                    as="p"
-                    className="text-red-500 text-sm"
-                  />
-                </div>
+                  <div className="mb-4">
+                    <input
+                      id="name"
+                      type="text"
+                      placeholder="name"
+                      className="w-full p-3 rounded-full border-2 border-gray-300 text-gray-800 focus:outline-none focus:border-darkorange"
+                      {...register("name", { required: "name is required." })}
+                    />
+                    <ErrorMessage
+                      errors={errors}
+                      name="name"
+                      as="p"
+                      className="text-red-500 text-sm"
+                    />
+                  </div>
 
-                <div className="mb-6">
-                  <input
-                    id="password"
-                    type="password"
-                    placeholder="password"
-                    className="w-full p-3 rounded-full border-2 border-gray-300 text-gray-800 focus:outline-none focus:border-darkorange"
-                    {...register("password", {
-                      required: "password is required",
-                      minLength: {
-                        value: 8,
-                        message: "minimun is 8 characters.",
-                      },
-                    })}
-                  />
-                  <ErrorMessage
-                    errors={errors}
-                    name="password"
-                    as="p"
-                    className="text-red-500 text-sm"
-                  />
-                </div>
+                  <div className="mb-4">
+                    <input
+                      id="email"
+                      type="email"
+                      placeholder="email"
+                      className="w-full p-3 rounded-full border-2 border-gray-300 text-gray-800 focus:outline-none focus:border-darkorange"
+                      {...register("email", {
+                        required: "Email is required.",
+                        pattern: {
+                          value:
+                            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                          message: "Email format is incorrect.",
+                        },
+                      })}
+                    />
+                    <ErrorMessage
+                      errors={errors}
+                      name="email"
+                      as="p"
+                      className="text-red-500 text-sm"
+                    />
+                  </div>
+
+                  <div className="mb-6">
+                    <input
+                      id="password"
+                      type="password"
+                      placeholder="password"
+                      className="w-full p-3 rounded-full border-2 border-gray-300 text-gray-800 focus:outline-none focus:border-darkorange"
+                      {...register("password", {
+                        required: "password is required",
+                        minLength: {
+                          value: 8,
+                          message: "minimun is 8 characters.",
+                        },
+                      })}
+                    />
+                    <ErrorMessage
+                      errors={errors}
+                      name="password"
+                      as="p"
+                      className="text-red-500 text-sm"
+                    />
+                  </div>
+
+                  <span className="flex gap-3">
+                    <button
+                      disabled={mutation.isPending}
+                      type="submit"
+                      className="w-full font-medium py-3 px-4 bg-darkorange hover:bg-hoverlighttext text-white rounded-full transition-colors"
+                    >
+                      {mutation.isPending
+                        ? "還在pend可以放icon"
+                        : "儲存所有更新"}
+                    </button>
+
+                    <button
+                      disabled={mutation.isPending}
+                      type="submit"
+                      className="w-full font-medium py-3 px-4 border-2 border-darkorange text-darkblue  hover:bg-hoverlighttext hover:text-white hover:border-hoverlighttext rounded-full transition-colors"
+                    >
+                      取消所有更新
+                    </button>
+                  </span>
+                </form>
+              </div>
+            </div>
+            {/* </div> */}
+          </div>
+        </TabsContent>
+
+        {/* =================================================================================== */}
+        {/* =================================================================================== */}
+
+        <TabsContent
+          className=" xl-24 md:w-[600px] xl:w-[700px] "
+          value="accountandarticle"
+        >
+          <div className="flex tablet:flex-row flex-col justify-center items-center tablet:items-start box-content bg-white rounded-lg shadow-md">
+            {/* 放大頭照 */}
+
+            <span className="px-12 pt-12 md:p-12">
+              <img
+                className="w-40 rounded-full object-cover bg-gray-300 flex items-center justify-center relative"
+                src={currentUser?.avatar}
+                alt="profile_pic"
+              />
+            </span>
+
+            {/* <div> */}
+            <div className="flex items-center justify-center mt-[10px] w-full">
+              {/* <div className="px-8 pt-3 pb-16 md:p-8 w-full max-w-md"> */}
+              {/* <form onSubmit={handleSubmit(onSubmit)}> */}
+              {/* <Button variant="outline" size="sm">
+                  新增房源
+                </Button>
+
+                <Button variant="outline" size="sm">
+                  房源列表
+                </Button> */}
+
+              {/* <div className="flex items-center justify-center mt-[10px] w-full">
+              <div className="px-8 pt-3 pb-16 md:p-8 w-full max-w-md">
+                <h1 className="text-4xl font-bold text-gray-700 mb-8"> */}
+
+              <div className="flex gap-6 flex-col justify-center items-center tablet:items-start px-8 pt-3 pb-16 md:p-8 w-full max-w-md">
+                <h1 className="text-gray-600 text-2xl font-extrabold">
+                  物件管理
+                </h1>
+                <button
+                  disabled={mutation.isPending}
+                  type="submit"
+                  className="w-3/4 font-medium py-3 px-4 bg-darkorange hover:bg-hoverlighttext text-white rounded-full transition-colors"
+                >
+                  新增房源
+                </button>
 
                 <button
                   disabled={mutation.isPending}
                   type="submit"
-                  className="w-full font-medium py-3 px-4 bg-darkorange hover:bg-hoverlighttext text-white rounded-full transition-colors"
+                  className="w-3/4  font-medium py-3 px-4 border-2 border-darkorange text-darkblue  hover:bg-hoverlighttext hover:text-white hover:border-hoverlighttext rounded-full transition-colors"
                 >
-                  {mutation.isPending ? "還在pend可以放icon" : "Sign up"}
+                  查看房源
                 </button>
-              </form>
 
-              <div className="text-center mt-6 text-gray-500 text-sm">
-                {mutation.isPending ? (
-                  <span className="text-gray-700 ml-1">
-                    <span className="text-blue-700">
-                      You already have an account? sign in
-                    </span>
-                  </span>
-                ) : (
-                  <Link to={"/sign-in"} className="text-gray-700 ml-1">
-                    <span className="text-blue-700">
-                      You already have an account? sign in
-                    </span>
-                  </Link>
-                )}
+                <p className="h-[0.8px] w-full bg-slate-500/30"></p>
+                <h1 className="text-gray-600 text-2xl font-extrabold">
+                  帳戶管理
+                </h1>
+                <span className="flex gap-3">
+                  <Dialog>
+                    <DialogTrigger>
+                      <Button variant="signoutmode" size="sm">
+                        登出帳戶
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Are you absolutely sure?</DialogTitle>
+                        <DialogDescription>
+                          This action cannot be undone. This will permanently
+                          delete your account and remove your data from our
+                          servers.
+                        </DialogDescription>
+                      </DialogHeader>
+                    </DialogContent>
+                  </Dialog>
+
+                  {/* =================== */}
+
+                  <Dialog>
+                    <DialogTrigger>
+                      <Button variant="deletemode" size="sm">
+                        刪除帳戶
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Are you absolutely sure?</DialogTitle>
+                        <DialogDescription>
+                          This action cannot be undone. This will permanently
+                          delete your account and remove your data from our
+                          servers.
+                        </DialogDescription>
+                      </DialogHeader>
+                    </DialogContent>
+                  </Dialog>
+                </span>
               </div>
+
+              {/* </form> */}
+              {/* </div> */}
             </div>
+            {/* </div> */}
           </div>
-          {/* </div> */}
-        </div>
-      </TabsContent>
-      <TabsContent value="accountandarticle">帳戶與文章</TabsContent>
-      {/* <TabsContent value="article">文章</TabsContent> */}
-    </Tabs>
+        </TabsContent>
+        {/* <TabsContent value="article">文章</TabsContent> */}
+      </Tabs>
+    </div>
   );
 };
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,24 +1,22 @@
-
-
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		fontSize: {
-  			clamp: 'clamp(1.5rem, 3vw, 1.5rem)'
-  		},
-  		screens: {
-  			mobile: '375px',
-			tablet: "600px"
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
+    extend: {
+      fontSize: {
+        clamp: "clamp(1.5rem, 3vw, 1.5rem)",
+      },
+      screens: {
+        mobile: "375px",
+        tablet: "600px",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
         // foreground: {
         //   DEFAULT: "#000000",
         //   dark: "#ffffff", // 在 dark mode 下變成白色
@@ -27,48 +25,56 @@ export default {
         darkorange: "#FFBB30",
         hoverlighttext: "#FF9100",
         darkblue: "#141E36",
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+        delete: {
+          DEFAULT: "hsl(var(--delete))",
+          foreground: "hsl(var(--delete-foreground))",
+        },
+        signout: {
+          DEFAULT: "hsl(var(--signout))",
+          foreground: "hsl(var(--signout-foreground))",
+        },
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
