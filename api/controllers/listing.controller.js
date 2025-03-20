4f21134175f91b22e4a927153bf12bbfe0082c5f
SolWatermelon <iamwatermelon0430@gmail.com>
2025-03-20 16:15:43 +0800

    [feat] create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -2,9 +2,10 @@ import Listing from "../models/listing.model.js";
 import dotenv from "dotenv";
 import cloudinary from "cloudinary";
 dotenv.config();
+import {errorHandler} from "../utils/error.js"
+
 export const createListing = async (req, res, next) => {
   try {
-
     const listing = await Listing.create(req.body.reqData);
     return res.status(200).json(listing);
   } catch (e) {
@@ -53,26 +54,22 @@ export const deleteListing = async (req, res, next) => {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+    api_secret: process.env.CLOUDINARY_API_SECRET,
   });
 
-  try {
-    const id = req.params.id;
-    // console.log("id", id);
-    const { imageUrls } = req.body;
-    // console.log("req.body",req.body)
-    console.log("imageUrls", imageUrls);
+  if (!req.params.id) return next(errorHandler(404, "listing not found"));
+  if (req.user.id !== req.params.id)
+    return errorHandler(401, "u can only delete ur own listing");
 
-    if (!id) return next(errorHandler(404, "listing not found"));
-    if (req.user.id !== id)
-      return errorHandler(401, "u can only delete ur own listing");
+  try {
+    const { imageUrls } = req.body;
 
     const deletingImgs = imageUrls.map((url) => {
       return cloudinary.v2.uploader.destroy(`${url.publicID}`);
     });
     const deletingImgsResult = await Promise.allSettled(deletingImgs);
 
-    const updatedUser = await Listing.findByIdAndDelete(id);
+    const updatedUser = await Listing.findByIdAndDelete(req.params.id);
 
     if (!updatedUser) {
       return res.status(404).json({ message: "listing can not be deleted" });
@@ -80,5 +77,19 @@ export const deleteListing = async (req, res, next) => {
     return res.status(200).json({ msg: "listing has been deleted" });
   } catch (e) {
     next(e);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) return next(errorHandler(404, "listing not found"));
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(404, "lu can only update ur own listing"));
+  }
+  try{
+    const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    return res.status(200).json(updatedListing);
+  }catch(e){
+    next(e)
   }
 };
