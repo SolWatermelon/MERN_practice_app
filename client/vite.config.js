9a6526b73548ee12623ae5c0985a6d02948395e8
SolWatermelon <iamwatermelon0430@gmail.com>
2025-03-31 01:54:34 +0800

    [feat] includes dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,8 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    port: process.env.PORT || 3000, // 讓Render使用環境變數PORT
+    // host: '0.0.0.0', // 允許外部存取
     // 解決cors問題
     proxy: {
       "/api": {
@@ -14,6 +16,9 @@ export default defineConfig({
         secure: true,
       },
     },
+  },
+  build: {
+    outDir: 'dist', // 確保Vite輸出到 dist
   },
   plugins: [react()],
   resolve: {
